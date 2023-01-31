b27af9a18de6ee14f99eba3d712b7553c16142ad
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2023-01-30 21:03:58 -0300

    fix: add unmerged drivent files

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,9 +2,5 @@ export * from "./users-router";
 export * from "./authentication-router";
 export * from "./events-router";
 export * from "./enrollments-router";
-<<<<<<< HEAD
-export * from "./tickets-router";
-=======
 export * from "./tickets-router";
 export * from "./payments-router";
->>>>>>> 6146a32e37cdc6604449613bf54b8daaf83717bb
